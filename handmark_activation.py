c726fac228954a8ebab15ff80363a23f4b63c566
Eric Harwood <eharwood@uchicago.edu>
2025-08-30 12:47:50 -0400

    hand classifer task working

--- handmark_activation.py
+++ handmark_activation.py
@@ -1,4 +1,5 @@
 import argparse
+import itertools, copy, csv
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -19,3 +20,95 @@ def get_args():
     args = parser.parse_args()
 
     return args
+
+def calc_landmark_list(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_point = []
+
+    # Keypoint
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+        # landmark_z = landmark.z
+
+        landmark_point.append([landmark_x, landmark_y])
+
+    return landmark_point
+
+def pre_process_landmark(landmark_list):
+    temp_landmark_list = copy.deepcopy(landmark_list)
+
+    # Convert to relative coordinates
+    base_x, base_y = 0, 0
+    for index, landmark_point in enumerate(temp_landmark_list):
+        if index == 0:
+            base_x, base_y = landmark_point[0], landmark_point[1]
+
+        temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+        temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+
+    # Convert to a one-dimensional list
+    temp_landmark_list = list(
+        itertools.chain.from_iterable(temp_landmark_list))
+
+    # Normalization
+    max_value = max(list(map(abs, temp_landmark_list)))
+
+    def normalize_(n):
+        return n / max_value
+
+    temp_landmark_list = list(map(normalize_, temp_landmark_list))
+
+    return temp_landmark_list
+
+
+def pre_process_point_history(image, point_history):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    temp_point_history = copy.deepcopy(point_history)
+
+    # Convert to relative coordinates
+    base_x, base_y = 0, 0
+    for index, point in enumerate(temp_point_history):
+        if index == 0:
+            base_x, base_y = point[0], point[1]
+
+        temp_point_history[index][0] = (temp_point_history[index][0] -
+                                        base_x) / image_width
+        temp_point_history[index][1] = (temp_point_history[index][1] -
+                                        base_y) / image_height
+
+    # Convert to a one-dimensional list
+    temp_point_history = list(
+        itertools.chain.from_iterable(temp_point_history))
+
+    return temp_point_history
+
+
+def logging_csv(number, mode, landmark_list, point_history_list):
+    if mode == 0:
+        pass
+    if mode == 1 and (0 <= number <= 9):
+        csv_path = 'model/keypoint_classifier/keypoint.csv'
+        with open(csv_path, 'a', newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([number, *landmark_list])
+    if mode == 2 and (0 <= number <= 9):
+        csv_path = 'model/point_history_classifier/point_history.csv'
+        with open(csv_path, 'a', newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([number, *point_history_list])
+    return
+
+def select_mode(key, mode):
+    number = -1
+    if 48 <= key <= 57:  # 0 ~ 9
+        number = key - 48
+    if key == 110:  # n
+        mode = 0
+    if key == 107:  # k
+        mode = 1
+    if key == 104:  # h
+        mode = 2
+    return number, mode
--- medusa_hands.py
+++ medusa_hands.py
@@ -2,17 +2,53 @@
 # from eyetrax import GazeEstimator, run_9_point_calibration
 # from mediapipe.framework.formats import landmark_pb2
 import cv2
+import copy, sys, os
 
 import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 from mediapipe import solutions as mp_solutions
+
 from handmark_activation import *
+
+from collections import deque, Counter
+
+from utils import CvFpsCalc
+from model import KeyPointClassifier
+from model import PointHistoryClassifier
 
 mp_hands = mp.solutions.hands
 
-def hand_track(display=False, camera=1):
+base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
+options = vision.GestureRecognizerOptions(base_options=base_options)
+recognizer = vision.GestureRecognizer.create_from_options(options)
+
+def hand_track(display=False, camera=0):
+
     #setup the model
     cap = cv2.VideoCapture(camera) 
     args = get_args()
+
+    keypoint_classifier = KeyPointClassifier()
+    point_history_classifier = PointHistoryClassifier()
+
+    with open('model/keypoint_classifier/keypoint_classifier_label.csv',
+              encoding='utf-8-sig') as f:
+        keypoint_classifier_labels = csv.reader(f)
+        keypoint_classifier_labels = [
+            row[0] for row in keypoint_classifier_labels
+        ]
+    with open(
+            'model/point_history_classifier/point_history_classifier_label.csv',
+            encoding='utf-8-sig') as f:
+        point_history_classifier_labels = csv.reader(f)
+        point_history_classifier_labels = [
+            row[0] for row in point_history_classifier_labels
+        ]
+
+    history_length = 16
+    point_history = deque(maxlen=history_length)
+    finger_gesture_history = deque(maxlen=history_length)
 
     cap_device = args.device
     cap_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -33,30 +69,79 @@ def hand_track(display=False, camera=1):
         print("Error: Could not open webcam."); return
     if display:
         cv2.namedWindow("Hand", cv2.WINDOW_NORMAL)
-
-
+        
     #Main Loop
     while True:
+
+        #for modes! very smart implementation here: https://github.com/kinivi/hand-gesture-recognition-mediapipe?tab=readme-ov-file
+        mode = 0
+        key = cv2.waitKey(10)
+        if key == 27:  # ESC
+            break
+        number, mode = select_mode(key, mode)
+
+        #start the camera
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        debug_image = copy.deepcopy(frame)
 
         if not ret:
             print("Error: Failed to read frames with cv2.")
             break
         
         #TODO: Hand Tracking
-        frame_analysis = model.process(rgb_frame)
-        if frame_analysis.multi_hand_landmarks is not None:
-            for landmark in frame_analysis.multi_hand_landmarks:
-                mp_solutions.drawing_utils.draw_landmarks(
-                    frame, landmark, mp_hands.HAND_CONNECTIONS)
-        
+        frame_analysis = model.process(frame)
 
-        #TODO: hand tracking: figure out which hand movements and how to SOTA detection (it should feel as good as apple)
+        if frame_analysis.multi_hand_landmarks is not None:
+            for hand_landmarks, handedness in zip(frame_analysis.multi_hand_landmarks,
+                      frame_analysis.multi_handedness):
+
+                #draw da hands
+                mp.solutions.drawing_utils.draw_landmarks(
+                    frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                
+                landmark_list = calc_landmark_list(debug_image, hand_landmarks)
+
+                # Conversion to relative coordinates / normalized coordinates
+                pre_processed_landmark_list = pre_process_landmark(
+                    landmark_list)
+                
+                pre_processed_point_history_list = pre_process_point_history(
+                    debug_image, point_history)
+                
+                # Write to the dataset file
+                logging_csv(number, mode, pre_processed_landmark_list,
+                            pre_processed_point_history_list)
+
+                # Hand sign classification
+                hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+                print(hand_sign_id)
+
+                #TODO: !!!!!!!!!!!!!! This is how we're going to lock the FUCK into this
+                if hand_sign_id != 0:  # Point gesture
+                    point_history.append(landmark_list[8])
+                else:
+                    point_history.append([0, 0])
+
+                # Finger gesture classification
+                finger_gesture_id = 0
+                point_history_len = len(pre_processed_point_history_list)
+                if point_history_len == (history_length * 2):
+                    finger_gesture_id = point_history_classifier(
+                        pre_processed_point_history_list)
+
+                # Calculates the gesture IDs in the latest detection
+                finger_gesture_history.append(finger_gesture_id)
+                most_common_fg_id = Counter(
+                    finger_gesture_history).most_common()
+                
+                print(finger_gesture_history)
+
+        else:
+            point_history.append([0, 0])
 
         if display:
-            cv2.imshow('Eye Controlled Mouse', frame)
+            cv2.imshow('Eye Controlled Mouse', debug_image)
             cv2.waitKey(1)
 
     cap.release()
@@ -67,6 +152,4 @@ if __name__ == "__main__":
     cam = 1
     if not cv2.VideoCapture(cam).isOpened():
         cam = 0
-
-    hand_track(display=True, camera=cam)                    # set to true to show image for debugging
-    print("hi")
+    hand_track(display=True, camera=0) #conver to cam                           # set to true to show image for debugging
